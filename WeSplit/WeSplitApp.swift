16fedad5fecd18e7ec91ea5003f69d938c7ff1fa
Gabriel Henrique <gabhenrique0@gmail.com>
2022-01-25 20:00:59 -0300

    :wrench: Chore: create project with SwiftUI structure

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriel Pereira on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
